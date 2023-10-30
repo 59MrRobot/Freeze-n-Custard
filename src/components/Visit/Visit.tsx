9a69c4da9ccd1d4202adcc9d963342721d70e118
Swazi Kunene <kuneneswazi18@gmail.com>
2023-10-30 20:19:49 +0200

    completed store locator

--- src/components/Visit/Visit.tsx
+++ src/components/Visit/Visit.tsx
@@ -9,34 +9,43 @@ export const Visit: React.FC = () => {
 
         <p>Dive into the world of Frosty Delight and discover why our frozen custard is the talk of the town.</p>
 
-        <p>[🛍️ Shop Now] [📍 Store Locator]</p>
+        <div className="visit__container">
+          <iframe
+            src="https://locatestore.com/RpGipr"
+            style={{ border: "none", width: "50%", height: "auto" }}
+            allow="geolocation"
+            title="map"
+          ></iframe>
 
-        <div className='visit__newsletter'>
-          <h3 className='visit__newsletter-title'>Join the newsletter!</h3>
+          <div className='visit__newsletter'>
+            <div>
+              <h3 className='visit__newsletter-title'>Join the newsletter!</h3>
 
-          <p>Subscribe to get the latest frosty news by email.</p>
+              <p>Subscribe to get the latest frosty news by email.</p>
+            </div>
 
-          <form className='form' onSubmit={(event) => event.preventDefault()}>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className='form__input'
-              placeholder='Your first name'
-            />
-            <input
-              type="text"
-              name="email"
-              id="email"
-              className='form__input'
-              placeholder='Your email address'
-            />
-            <input
-              type="submit"
-              value="Subscribe"
-              className='form__button'
-            />
-          </form>
+            <form className='form' onSubmit={(event) => event.preventDefault()}>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className='form__input'
+                placeholder='Your first name'
+              />
+              <input
+                type="text"
+                name="email"
+                id="email"
+                className='form__input'
+                placeholder='Your email address'
+              />
+              <input
+                type="submit"
+                value="Subscribe"
+                className='form__button'
+              />
+            </form>
+          </div>
         </div>
       </div>
   </section>
