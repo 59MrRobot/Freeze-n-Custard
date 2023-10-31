1fca0a4551d731efd3d576853752c48ccec3c936
Swazi Kunene <kuneneswazi18@gmail.com>
2023-10-31 02:46:03 +0200

    made responsive for mobile and tablets

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 export const Header: React.FC = React.memo(
   () => {
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
       <header className='header' id='header'>
-        <Link to="/">
-          <img
-            src={`${process.env.PUBLIC_URL}/images/logo.png`}
-            alt="logo"
-            className="logo"
-          />
-        </Link>
+        {!showMenu && (
+          <Link to="/">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/logo.png`}
+              alt="logo"
+              className="logo"
+            />
+          </Link>
+        )}
 
         <nav className='nav'>
           <ul className='list'>
@@ -34,6 +40,64 @@ export const Header: React.FC = React.memo(
               </HashLink>
             </ul>
         </nav>
+
+        {!showMenu && (
+          <div className='header__open-menu'>
+            <MenuIcon
+              sx={{ cursor: 'pointer' }}
+              onClick={() => setShowMenu(true)}
+            />
+          </div>
+        )}
+        
+        <div className={`menu menu--${showMenu ? 'show' : 'hide'}`}>
+          <div className='menu__controls'>
+            <Link to="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/images/logo.png`}
+                alt="logo"
+                className="logo"
+              />
+            </Link>
+
+            {showMenu && (
+              <CloseIcon
+                sx={{ cursor: 'pointer' }}
+                onClick={() => setShowMenu(false)}
+              />
+            )}
+          </div>
+
+          <nav className='nav nav--mobile'>
+            <ul className='list list--mobile'>
+                <HashLink to="/#about" className='nav__link'>
+                  <li className='list__item' onClick={() => setShowMenu(false)}>
+                    About
+                  </li>
+                </HashLink>
+                <HashLink to="/#creations" className='nav__link'>
+                  <li className='list__item' onClick={() => setShowMenu(false)}>
+                    Signature Creations
+                  </li>
+                </HashLink>
+                <HashLink to="/#visit-us" className='nav__link'>
+                  <li className='list__item' onClick={() => setShowMenu(false)}>
+                    Visit Us
+                  </li>
+                </HashLink>
+                <HashLink to="/#reviews" className='nav__link'>
+                  <li className='list__item' onClick={() => setShowMenu(false)}>
+                    Reviews
+                  </li>
+                </HashLink>
+                <HashLink to="/#join-us" className='nav__link'>
+                  <li className='list__item' onClick={() => setShowMenu(false)}>
+                    Join Us
+                  </li>
+                </HashLink>
+              </ul>
+          </nav>
+        </div>
       </header>
     )
   }
